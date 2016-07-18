9c79e2f3f8480a136bbf6db8befb24ab8b0c366c
cflavs <cflavs.7@gmail.com>
2016-07-18 16:01:42 -0300

    Modelo do Users Schema

---
+++ models/users.js
@@ -0,0 +1,16 @@
+var mongoose  = require('mongoose');
+var userSchema = mongoose.schema;
+
+//defines the users' mongoose Schema
+var userSchema = new Schema({
+  name    : {type: String, required:true},
+  userId  : {type: String, required:true},
+  password: {type: String, required:true},
+  email   : {type: String, required:true},
+  created : {type: Date,  default: Date.now },
+  isActive: Boolean
+});
+
+var User = mongoose.model('User', userSchema);
+
+module.exports = User;
