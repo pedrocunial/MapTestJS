74a7e164c6fb6e79b2d3fb6473b04e822e22bb3a
pedrocunial <pedrocc4@al.insper.edu.br>
2016-07-18 10:35:39 -0300

    Index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+// index.js
+
+// Dependencies
+var express        = require('express');
+var mongoose       = require('mongoose');
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
+
+var app = express();
+
+// Middleware
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(methodOverride('X-HTTP-Method-Override'));
+
+// Mongoose
+mongoose.connect("mongodb://localhost/map_test");
+mongoose.connection.once('open', function() {
+  app.listen(3000);
+  console.log('listening to port 3000');
+});
