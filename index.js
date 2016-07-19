222a1c9ce045f856139c2c0e497cbd4a3cec22db
cflavs <cflavs.7@gmail.com>
2016-07-19 16:33:48 -0300

    Renomeei o arquivo de rota do usuário

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var express        = require('express');
 var mongoose       = require('mongoose');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
-
-var routes = require("./routers/index");
+//Routes
+var routes = require("./routers/user_routes");
 
 var app = express();
 
